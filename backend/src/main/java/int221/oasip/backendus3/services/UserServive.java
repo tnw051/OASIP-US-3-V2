6ce29d15d0686f88033f50d1483c2ed0665099a3
Rooted <modhanami@gmail.com>
2022-08-18 21:33:25 +0700

    Sort user by name on backend

--- backend/src/main/java/int221/oasip/backendus3/services/UserServive.java
+++ backend/src/main/java/int221/oasip/backendus3/services/UserServive.java
@@ -5,6 +5,7 @@ import int221.oasip.backendus3.entities.User;
 import int221.oasip.backendus3.repository.UserRepository;
 import int221.oasip.backendus3.utils.ModelMapperUtils;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,7 +17,7 @@ public class UserServive {
     private ModelMapperUtils modelMapperUtils;
 
     public List<UserResponse> getAll() {
-        List<User> users = repository.findAll();
+        List<User> users = repository.findAll(Sort.by("name"));
         return modelMapperUtils.mapList(users, UserResponse.class);
     }
 }
