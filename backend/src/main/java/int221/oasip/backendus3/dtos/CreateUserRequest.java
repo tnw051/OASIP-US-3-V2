a9d1801ee132e96425f52cd23bf1232392c4e7bd
Rooted <modhanami@gmail.com>
2022-08-24 00:35:03 +0700

    Customize email regex to allow leading and trailing spaces for CreateUserRequest

--- backend/src/main/java/int221/oasip/backendus3/dtos/CreateUserRequest.java
+++ backend/src/main/java/int221/oasip/backendus3/dtos/CreateUserRequest.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -20,7 +19,9 @@ public class CreateUserRequest {
 
     @NotBlank(message = "Email must not be blank")
     @Size(max = 50, message = "Email must be less than {max} characters")
-    @Email(message = "Email is invalid")
+    // extended java email regex by allowing leading and trailing whitespace
+    // NOTE: @Email does not seem to work with the custom regex
+    @Pattern(message = "Email is invalid", regexp = "\\s*(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])\\s*")
     private String email;
 
     private String role = Role.STUDENT.toString();
