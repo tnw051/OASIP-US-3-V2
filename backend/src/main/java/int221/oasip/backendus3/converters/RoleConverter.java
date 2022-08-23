cc81829f772e71f01190b59977c325309f8bdad8
Rooted <modhanami@gmail.com>
2022-08-24 00:30:03 +0700

    Comment out RoleConverter

--- backend/src/main/java/int221/oasip/backendus3/converters/RoleConverter.java
+++ backend/src/main/java/int221/oasip/backendus3/converters/RoleConverter.java
@@ -5,21 +5,21 @@ import int221.oasip.backendus3.entities.Role;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
-@Converter(autoApply = true)
-public class RoleConverter implements AttributeConverter<Role, String> {
-    @Override
-    public String convertToDatabaseColumn(Role role) {
-        if (role == null) {
-            return null;
-        }
-        return role.toString();
-    }
-
-    @Override
-    public Role convertToEntityAttribute(String role) {
-        if (role == null) {
-            return null;
-        }
-        return Role.fromString(role);
-    }
-}
+//@Converter(autoApply = false)
+//public class RoleConverter implements AttributeConverter<Role, String> {
+//    @Override
+//    public String convertToDatabaseColumn(Role role) {
+//        if (role == null) {
+//            return null;
+//        }
+//        return role.toString();
+//    }
+//
+//    @Override
+//    public Role convertToEntityAttribute(String role) {
+//        if (role == null) {
+//            return null;
+//        }
+//        return Role.fromString(role);
+//    }
+//}
