e591e1eacb1eca1136bcc2efe211bb9207904662
manassinee <manassinee.v@mail.kmutt.ac.th>
2022-08-28 22:52:39 +0700

    Add an endpoint for password matching on backend

---
+++ backend/src/main/java/int221/oasip/backendus3/controllers/AuthController.java
@@ -0,0 +1,34 @@
+package int221.oasip.backendus3.controllers;
+
+import int221.oasip.backendus3.dtos.MatchRequest;
+import int221.oasip.backendus3.exceptions.EntityNotFoundException;
+import int221.oasip.backendus3.services.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
+public class AuthController {
+    private AuthService service;
+
+    @PostMapping("/match")
+    public String match(@Valid @RequestBody MatchRequest matchRequest) {
+        try {
+            boolean matches = service.match(matchRequest);
+            if (!matches) {
+                return "Password NOT Matched";
+            }
+            return "Password Matched";
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+}
---
+++ backend/src/main/java/int221/oasip/backendus3/dtos/MatchRequest.java
@@ -0,0 +1,23 @@
+package int221.oasip.backendus3.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+public class MatchRequest {
+    @NotBlank(message = "Email must not be blank")
+    @Size(max = 50, message = "Email must be less than {max} characters")
+    // extended java email regex by allowing leading and trailing whitespace
+    // NOTE: @Email does not seem to work with the custom regex
+    @Pattern(message = "Email is invalid", regexp = "\\s*(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])\\s*")
+    private String email;
+
+    @NotBlank(message = "Password must not be blank")
+    @Size(min = 8, max = 14, message = "Password must be between {min} and {max} characters")
+    private String password;
+}
