58d82eb008521292f09d13eb585178fed4eadcc3
manassinee <manassinee.v@mail.kmutt.ac.th>
2022-09-05 21:33:39 +0700

    Allow only authenticated requests to GET /users

--- backend/src/main/java/int221/oasip/backendus3/configs/ResourceServerConfig.java
+++ backend/src/main/java/int221/oasip/backendus3/configs/ResourceServerConfig.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -39,6 +40,7 @@ public class ResourceServerConfig {
         return http
                 .authorizeHttpRequests()
                 .antMatchers("/api/auth/private").authenticated()
+                .antMatchers(HttpMethod.GET, "/api/users").authenticated()
                 .anyRequest().permitAll()
                 .and()
                 .csrf().disable()
