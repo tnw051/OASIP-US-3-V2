0bedd1c52dce8f1cb324ad0d4b175acf68a8309a
Rooted <modhanami@gmail.com>
2022-10-06 00:16:40 +0700

    Format App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,16 @@
 <script setup>
-import { useRouter } from 'vue-router';
-import { useAuth } from './utils/useAuth';
+import { useRouter } from "vue-router";
+import { useAuth } from "./utils/useAuth";
 
-const router = useRouter()
+const router = useRouter();
 const { logout, isAdmin, isLecturer, isAuthenticated, user } = useAuth();
 
 async function handleLogout() {
   const success = await logout();
   if (success) {
-    router.push({ name: 'login' })
+    router.push({ name: "login" });
   } else {
-    alert('Something went wrong')
+    alert("Something went wrong");
   }
 }
 </script>
@@ -18,25 +18,70 @@ async function handleLogout() {
 <template>
   <nav class="bg-white px-12 py-4 flex border-b border-gray-200 justify-between items-center">
     <div class="flex items-center gap-1 text-sm font-medium">
-      <img src="https://cdn.7tv.app/emote/611cb0c5f20f644c3fadb992/3x" width="64" />
-      <img src="https://cdn.betterttv.net/emote/60a21baf67644f1d67e87a6c/3x" width="64" />
-      <router-link to="/" class="ml-6 p-2 rounded-md text-gray-700 hover:text-sky-600">Events</router-link>
-      <router-link v-if="!isLecturer" :to="{ name: 'createEvent' }" class="p-2 rounded-md text-gray-700 hover:text-sky-600">Create
-        Event</router-link>
-      <router-link :to="{ name: 'categories' }" class="p-2 ml-2 text-gray-700 hover:text-sky-600">Categories
+      <img
+        src="https://cdn.7tv.app/emote/611cb0c5f20f644c3fadb992/3x"
+        width="64"
+      >
+      <img
+        src="https://cdn.betterttv.net/emote/60a21baf67644f1d67e87a6c/3x"
+        width="64"
+      >
+      <router-link
+        to="/"
+        class="ml-6 p-2 rounded-md text-gray-700 hover:text-sky-600"
+      >
+        Events
       </router-link>
-      <router-link v-if="isAdmin" :to="{ name: 'users' }" class="p-2 ml-2 text-gray-700 hover:text-sky-600">Users</router-link>
-      <router-link v-if="isAdmin" :to="{ name: 'createUser' }" class="p-2 ml-2 text-gray-700 hover:text-sky-600">Create User
+      <router-link
+        v-if="!isLecturer"
+        :to="{ name: 'createEvent' }"
+        class="p-2 rounded-md text-gray-700 hover:text-sky-600"
+      >
+        Create
+        Event
       </router-link>
-      <router-link v-if="!isAuthenticated" :to="{ name: 'login' }" class="p-2 ml-2 text-gray-700 hover:text-sky-600">Login</router-link>
+      <router-link
+        :to="{ name: 'categories' }"
+        class="p-2 ml-2 text-gray-700 hover:text-sky-600"
+      >
+        Categories
+      </router-link>
+      <router-link
+        v-if="isAdmin"
+        :to="{ name: 'users' }"
+        class="p-2 ml-2 text-gray-700 hover:text-sky-600"
+      >
+        Users
+      </router-link>
+      <router-link
+        v-if="isAdmin"
+        :to="{ name: 'createUser' }"
+        class="p-2 ml-2 text-gray-700 hover:text-sky-600"
+      >
+        Create User
+      </router-link>
+      <router-link
+        v-if="!isAuthenticated"
+        :to="{ name: 'login' }"
+        class="p-2 ml-2 text-gray-700 hover:text-sky-600"
+      >
+        Login
+      </router-link>
     </div>
     <div class="text-sm text-gray-700">
-      <span v-if="isAuthenticated" class="font-medium">{{ user.sub }}</span>
-      <a @click="handleLogout" v-if="isAuthenticated" class="cursor-pointer p-2 ml-2 text-gray-700 hover:text-sky-600">Logout</a>
+      <span
+        v-if="isAuthenticated"
+        class="font-medium"
+      >{{ user.sub }}</span>
+      <a
+        v-if="isAuthenticated"
+        class="cursor-pointer p-2 ml-2 text-gray-700 hover:text-sky-600"
+        @click="handleLogout"
+      >Logout</a>
     </div>
   </nav>
 
-  <router-view></router-view>
+  <router-view />
 </template>
  
 <style scoped>
