8f580a34a4ea18c2baa6c94c78d80d9e350bbe13
Rooted <modhanami@gmail.com>
2022-10-12 11:18:11 +0700

    Disable vite dev server proxy's secure option

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
-      '/api': {
+      "/api": {
         target: "http://localhost:8080",
-        changeOrigin: true
-      }
-    }
-  }
-})
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
