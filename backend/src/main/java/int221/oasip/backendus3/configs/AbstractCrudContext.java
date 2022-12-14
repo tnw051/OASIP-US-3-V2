7fd15bbdf87b59ce8ce21153ea7dc629ae256622
Rooted <modhanami@gmail.com>
2022-12-14 11:31:06 +0700

    Remove dangling files

--- backend/src/main/java/int221/oasip/backendus3/configs/AbstractCrudContext.java
+++
@@ -1,2 +0,0 @@
-package int221.oasip.backendus3.configs;public class AbstractCrudContext {
-}
--- backend/src/main/java/int221/oasip/backendus3/configs/ContextAwareCrudAuthorizer.java
+++
@@ -1,5 +0,0 @@
-package int221.oasip.backendus3.configs;
-
-public interface ContextAwareAuthorizer<Resource, Context> {
-    void check(Resource resource, Context context);
-}
