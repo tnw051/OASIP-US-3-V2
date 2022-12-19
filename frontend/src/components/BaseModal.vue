0149dd511c6ba999cd8bfe00ba126b05c3c44b97
Rooted <modhanami@gmail.com>
2022-12-20 01:10:17 +0700

    Add missingg BaseModal

---
+++ frontend/src/components/BaseModal.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+  isOpen: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+defineEmits(["close"]);
+</script>
+ 
+<template>
+  <Teleport to="body">
+    <div
+      v-if="props.isOpen"
+      class="absolute top-0 left-0 z-50 grid h-screen w-screen place-content-center bg-black/50 text-slate-700 shadow-2xl"
+      @click="$emit('close')"
+    >
+      <slot />
+    </div>
+  </Teleport>
+</template>
+ 
+<style scoped>
+</style>
